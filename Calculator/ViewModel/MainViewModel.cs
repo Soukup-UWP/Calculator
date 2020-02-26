6b822516f242e44d31017bd5eb1a1a559c17f0d6
unknown <Administrator@PC2019-07.ad.pslib.cz>
2020-02-26 09:23:09 +0100

    MainViewModel without function

--- Calculator/ViewModel/MainViewModel.cs
+++ Calculator/ViewModel/MainViewModel.cs
@@ -8,5 +8,21 @@ namespace Calculator.ViewModel
 {
     class MainViewModel
     {
+        private string _input;
+        private string _storedValue;
+        private string _message;
+        public ParametrizedRelayCommand NumberButton { get; set; }
+        public ParametrizedRelayCommand OperationButton { get; set; }
+        public ParametrizedRelayCommand FunctionButton { get; set; }
+
+        public RelayCommand Reset { get; set; }
+        public RelayCommand Point { get; set; }
+        public RelayCommand Sign { get; set; }
+        public RelayCommand Result { get; set; }
+
+        public MainViewModel()
+        {
+
+        }
     }
 }
