061aed3474221eaf586980e2d46d169da566eb87
Soukupis <joseph.soukup@outlook.com>
2020-03-03 22:11:13 +0100

    Numbers, Sign, Reset and decimal point

--- Calculator/ViewModel/MainViewModel.cs
+++ Calculator/ViewModel/MainViewModel.cs
@@ -1,28 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Calculator.ViewModel
 {
-    class MainViewModel
+    class MainViewModel : INotifyPropertyChanged
     {
         private string _input;
         private string _storedValue;
         private string _message;
-        public ParametrizedRelayCommand NumberButton { get; set; }
+        public ParametrizedRelayCommand NumberButton{ get; set; } //DONE
         public ParametrizedRelayCommand OperationButton { get; set; }
         public ParametrizedRelayCommand FunctionButton { get; set; }
 
         public RelayCommand Reset { get; set; }
-        public RelayCommand Point { get; set; }
-        public RelayCommand Sign { get; set; }
+        public RelayCommand Point { get; set; } //DONE
+        public RelayCommand Sign { get; set; } //DONE
         public RelayCommand Result { get; set; }
+
+        
 
         public MainViewModel()
         {
+            TextMessage = "";
+            NumberButton = new ParametrizedRelayCommand(
+                (param) =>
+                {
+                    TextMessage += param;
+                },
+                () => { return true; }
+            );
+            Sign = new RelayCommand(
+                () =>
+                {
+                    if(TextMessage.Length > 0)
+                    {
+                        TextMessage = Convert.ToString((Convert.ToDouble(TextMessage) * (-1)));
+                    }
+                },
+                () => { return true; }
+            );
+            Point = new RelayCommand(
+                () =>
+                {
+                    if ((!TextMessage.Contains(".")) && TextMessage.Length > 0)
+                    {
+                        TextMessage += ".";
+                    }
+                },
+                () => { return true; }
+            );
+            Reset = new RelayCommand(
+                () =>
+                {
+                    TextMessage = "0";
+                },
+                () => { return true; }
+            );
+        }
+
+        public string TextMessage
+        {
+            get => _message;
+            set
+            {
+                _message = value;
+                NotifyPropertyChanged();
+            }
 
         }
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
